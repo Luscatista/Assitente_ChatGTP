f205352dfb6fa27f3577b23db0c8f28acdbaea13
Rosa Sianga <rosa.sianga@gmail.com>
2025-06-10 20:28:30 -0300

    Script criado

---
+++ script.js
@@ -0,0 +1,18 @@
+async function sendMessage() {
+    const chatBox = document.getElementById("chatBox");
+
+    const userInput = document.getElementById("userInput");
+
+    const userMessage = userInput.value;
+
+    if(!userMessage) return;
+
+    const userDiv = document.createElement("div");
+    userDiv.className = "user-message message"
+    userDiv.textContent = userMessage
+    chatBox.appendChild(userDiv);
+
+    userInput.value = "";
+
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
